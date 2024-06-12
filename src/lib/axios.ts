73edcd4cb3f662b4cf993f805c222d08df38664c
brl4528 <blgc.sgo@hotmail.com>
2024-06-12 14:39:57 -0400

    first commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,6 +5,10 @@ import { env } from '@/env'
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
+  headers: {
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDc5MzU5MDcsImV4cCI6MTcwODAyMjMwNywic3ViIjoiZmNmMTlhNDQtZmFiZC00ZTU4LTg5MjMtMmYxNmNlYmFiMGU5In0.rGaeqCf9vhNKs_HFVV9I2qXXixUFyoG-qn7M5WAgrTk',
+  },
 })
 
 if (env.VITE_ENABLED_API_DELAY) {
